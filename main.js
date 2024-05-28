6807c8a5579d696b3964ff89339e17fbc8e14a77
ARFHSI <Abdulraouf.hsi@gmail.com>
2024-05-28 15:03:50 +0300

    DOM-HW

---
+++ main.js
@@ -0,0 +1,13 @@
+
+let btn = document.getElementById('logo');
+function clickMe(){
+    text.replaceAll("dog","cat");
+    text.replaceAll("dogs","cats");
+    text.replaceAll("Dogs","Cats");
+
+
+}
+
+
+
+  
